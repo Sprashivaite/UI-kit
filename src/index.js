00786dc4f55e9195f1f9a0559dbcc020e1356385
Sprashivaite <sanitar.kem@mail.ru>
2020-02-19 03:16:06 +0700

    добавил слайдер

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styl/main.styl'
+
+// import $ from 'jquery'
+import ionRangeSlider from 'ion-rangeslider'
+// import '../node_modules/ion-rangeslider/css/ion.rangeSlider.css'
 import './pug/includes/range-slider/range-slider.js'
 // import "./pug/includes/Dropdown_guests/Dropdown__guests_expanded/Dropdown__guests_expanded.js"
