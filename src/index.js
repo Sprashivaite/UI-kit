8cca90a84c18fbb68fafd65cee70c867d5382b97
Sprashivaite <sanitar.kem@mail.ru>
2020-02-24 00:55:10 +0700

    сделал rate-button

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ import ionRangeSlider from 'ion-rangeslider'
 import './pug/includes/range-slider/range-slider.js'
 import "./pug/includes/expandable-checkbox-list/expandable-checkbox-list.js"
 import "./pug/includes/radio-button/radio-button.js"
+import "./pug/includes/rate-button/rate-button.js"
