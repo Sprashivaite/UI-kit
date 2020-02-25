201464890877138b036e2a55af767a2ad6a6d86c
Sprashivaite <sanitar.kem@mail.ru>
2020-02-26 03:55:14 +0700

    добавляю дропдаун

--- src/pug/includes/Dropdown_guests/Dropdown__guests_expanded.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $(".Dropdown-guests").click(function(){ // задаем функцию при нажатиии на элемент <button>
-	    $( ".Dropdown-guests" ).toggleClass( ".Dropdown__Field_expanded" ); // удаляем все элементы <p> из DOM
-	  });
-});
-
