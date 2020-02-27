980c5d356187d97c999c016d02e69341d5dc9223
Sprashivaite <sanitar.kem@mail.ru>
2020-02-27 10:32:09 +0700

    переделал радио-бтн

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ionRangeSlider from 'ion-rangeslider'
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
 import './pug/includes/range-slider/range-slider.js'
-import "./pug/includes/radio-button/radio-button.js"
 import "./pug/includes/rate-button/rate-button.js"
 import "./pug/includes/like-button/like-button.js"
 import "./pug/includes/pagination/pagination.js"
--- src/pug/includes/radio-button/radio-button.js
+++
@@ -1,5 +0,0 @@
-// $(function() {
-//     $(".radio-button").click(function(){ 
-//         $(".radio-button__input" ).toggle( ".radio-button__input_checked" );
-// 	  });
-// });
