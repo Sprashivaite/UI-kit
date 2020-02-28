c1a3b5195c58c0cf0ecc3b271e0e1a7fec88d2fa
Sprashivaite <sanitar.kem@mail.ru>
2020-02-28 12:46:42 +0700

    сделал кнопку очистить в dropdown

--- src/pug/includes/data-dropdown/data-dropdown.js
+++ src/pug/includes/data-dropdown/data-dropdown.js
@@ -1,20 +1,9 @@
 $(function() {
     $('#data-dropdown-1').datepicker({    
-        onSelect: function (fd, d, picker) { 
+        onSelect: function (fd) { 
             $("#data-dropdown-1").val(fd.split("-")[0]);
             $("#data-dropdown-2").val(fd.split("-")[1]);
         }
     })
     
 });
-
-
-// onSelect: (formattedDate) => {
-//             if (formattedDate.length > 1) {
-//             $('#data-dropdown-1').val(formattedDate.split(",")[0]);
-//             $('#data-dropdown-2').val(formattedDate.split(",")[1]);
-            
-//             this.applyButton.style.color = this.colorPurple;
-//             this.selectedDates = formattedDate;
-//             }
-//             }    
