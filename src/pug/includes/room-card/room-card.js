03c1c7964f7954e02e719c98848dde57a44e64fc
Sprashivaite <sanitar.kem@mail.ru>
2020-11-25 17:18:14 +0700

    длбавлена логика корусели для room card

--- src/pug/includes/room-card/room-card.js
+++ src/pug/includes/room-card/room-card.js
@@ -1,17 +1,32 @@
-// ;let picture = document.querySelectorAll(".room-card__button-circle")
+function changePicture(card) {
+  let buttonPictures = card.querySelector(".room-card__buttons").childNodes;
+  let contForPicture = card.querySelector(".room-card__pictures").childNodes;
 
+  function circleToggle(elem, parent) {
+    for (const item of parent) {
+      item.style.backgroundColor = "";
+    }
+    elem.style.backgroundColor = "white";        
+    for (const item of contForPicture) {
+            item.style.display = "none";
+          }
+    if (card.querySelector(".room-card__picture_" + elem.num)) {
 
-// console.log(picture);
+        card.querySelector(".room-card__picture_" + elem.num).style.display = "block";
+    }
+    else{card.querySelector(".room-card__picture_1").style.display = "block";}
+    // card.querySelector(".room-card__picture_" + elem.num).style.display = "block";
+  }
+  let numbers = 0;
+  for (const item of buttonPictures) {
+    item.num = ++numbers;
+    item.addEventListener("click", () => {
+      circleToggle(item, buttonPictures);
+    });
+  }
+}
 
-// function pictureToggle(elem){
-//     console.log(getComputedStyle(elem).backgroundColor);
-//             if(getComputedStyle(elem).backgroundColor == 'rgb(255, 255, 255)'){
-//             elem.style.backgroundColor = 'white'
-//         }
-//         else {elem.style.backgroundColor = ''}
-
-// }
-
-// for (const i of picture) {
-//     i.addEventListener('click', ()=>{ pictureToggle(i)})
-// }
+let cards = document.querySelectorAll(".room-card");
+for (const item of cards) {
+  changePicture(item);
+}
