7e1f8decd1a0f2c459db644d74fa6c60b8cb36bc
Sprashivaite <sanitar.kem@mail.ru>
2020-12-04 15:47:45 +0700

    правки отступов

--- src/pug/includes/rate-button/rate-button.js
+++ src/pug/includes/rate-button/rate-button.js
@@ -1,84 +1,22 @@
-/**
- * jQuery Star Rating plugin
- * Joost van Velzen - http://joost.in
- *
- * v 1.0.3
- *
- * cc - attribution + share alike
- * http://creativecommons.org/licenses/by-sa/4.0/
- */
+function starMaker(parent) {
+  let stars = parent.querySelector(".rate-button__star").children;
 
-(function ($) {
-    $.fn.addRating = function (options) {
-        var obj = this;
-        var settings = $.extend({
-            max: 5,
-            half: true,
-            fieldName: 'rating',
-            fieldId: 'rating',
-            icon: 'star',
-            selectedRatings:0
-        }, options);
-        this.settings = settings;
-
-        // create the stars
-        for (var i = 1; i <= settings.max; i++) {
-            var star = $('<i/>').addClass('material-icons').html(this.settings.icon + '_border').data('rating', i).appendTo(this).click(
-                function () {
-                    obj.setRating($(this).data('rating'));
-                }
-            ).hover(
-                function (e) {
-                    obj.showRating($(this).data('rating'), false);
-                }, function () {
-                    obj.showRating(0, false);
-                }
-            );
-
-        }
-        $(this).append('<input type="hidden" name="' + settings.fieldName + '" id="' + settings.fieldId + '" value="' + settings.selectedRatings + '" />');
-
-        obj.showRating(settings.selectedRatings, true);
-    };
-
-    $.fn.setRating = function (numRating) {
-        var obj = this;
-        $('#' + obj.settings.fieldId).val(numRating);
-        obj.showRating(numRating, true);
-    };
-
-    $.fn.showRating = function (numRating, force) {
-        var obj = this;
-        if ($('#' + obj.settings.fieldId).val() == '' || force) {
-            $(obj).find('i').each(function () {
-                var icon = obj.settings.icon + '_border';
-                $(this).removeClass('selected');
-
-                if ($(this).data('rating') <= numRating) {
-                    icon = obj.settings.icon;
-                    $(this).addClass('selected');
-                }
-                $(this).html(icon);
-            })
-        }
-    }
-
-}(jQuery));
-
-$(document).ready(function(){
-    $('.rate-button_1').addRating({
-        selectedRatings:1
+  for (const star of stars) {
+    star.addEventListener("click", () => {
+      for (const star of stars) {
+        star.innerHTML = "star_border";
+      }
+      star.innerHTML = "star";
+      let prevStar = star;
+      while (prevStar.previousElementSibling) {
+        prevStar.innerHTML = "star";
+        prevStar.previousElementSibling.innerHTML = "star";
+        prevStar = prevStar.previousElementSibling;
+      }
     });
-    $('.rate-button_2').addRating({
-        selectedRatings:2
-    });
-    $('.rate-button_3').addRating({
-        selectedRatings:3
-    });
-    $('.rate-button_4').addRating({
-        selectedRatings:4
-    });
-    $('.rate-button_5').addRating({
-        selectedRatings:5
-    });
-})
+  }
+}
+let rateButtons = document.querySelectorAll(".rate-button");
+for (const item of rateButtons) {
+  starMaker(item);
+}
