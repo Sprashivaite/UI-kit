e902753a417d8ba7bded21ace8b451e6e3dc643a
Sprashivaite <sanitar.kem@mail.ru>
2020-12-23 21:52:41 +0700

    логика сделана более независимой

--- src/pug/includes/dropdown-guests/dropdown-guests.js
+++ src/pug/includes/dropdown-guests/dropdown-guests.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.minus').click(function () {
+  $('.dropdown .minus').click(function () {
       var $input = $(this).parent().find('input');
       var count = parseInt($input.val()) - 1;
       count = count < 0 ? 0 : count;
@@ -7,7 +7,7 @@ $(document).ready(function () {
       $input.change();
       return false;
   });
-  $('.plus').click(function () {
+  $('.dropdown .plus').click(function () {
       var $input = $(this).parent().find('input');
       $input.val(parseInt($input.val()) + 1);
       $input.change();
@@ -18,7 +18,7 @@ $(document).ready(function () {
       cases = [2, 0, 1, 1, 1, 2];
       return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
   }  
-      $('.dropdown-content__box-button').find('button').click(function () {
+      $('.dropdown .dropdown-content__box-button').find('button').click(function () {
           let target = $(this).closest('.dropdown')
           let child = target.find('.child').val();
           let adult = target.find('.adult').val();
@@ -30,29 +30,27 @@ $(document).ready(function () {
              
           } else {target.find('.dropbtn__text').text(summ + ' ' + declOfNum(summ, ['гость', 'гостя', 'гостей']));target.find('.clear').css('opacity', '1');}
             
+          target.find('.clear').click(function () {
+            target.find('.clear').css('opacity', '0');
+              target.find('.dropbtn__text').text('Сколько гостей');
+              target.find('.child').val(0);
+              target.find('.adult').val(0);
+              target.find('.baby').val(0);
+          
+          });
+            target.find('.submit').click(function () {
+            target.find('.dropdown-content').removeClass('dropdown-content__show');
+   });
       });
  
-  function resizeInput() {
-      $(this).attr('size', $(this).val().length);
-  }
-  $('input[type="text"]') 
-      .keyup(resizeInput) 
-      .each(resizeInput); 
-  $('.dropdown__dropbtn').click(function () {
+  $('.dropdown .dropdown__dropbtn').click(function () {
       if($(this).next('.dropdown-content').hasClass('dropdown-content__show')){
           $(this).next('.dropdown-content').removeClass('dropdown-content__show');
-      } else $(this).next('.dropdown-content').addClass('dropdown-content__show');
+          $(this).find('i').css('transform', 'rotate(0deg)')
+      } else{
+           $(this).next('.dropdown-content').addClass('dropdown-content__show');
+           $(this).find('i').css('transform', 'rotate(180deg)')
+        }
 
   }); 
-  $('.dropdown__buttons-container .clear').click(function () {
-    $('.dropdown__buttons-container .clear').css('opacity', '0');
-      $('.dropbtn__text').text('Сколько гостей');
-      $('.child').val(0);
-      $('.adult').val(0);
-      $('.baby').val(0);
-  
-  });
-  $('.dropdown__buttons-container .submit').click(function () {
-      $('.dropdown-content').removeClass('dropdown-content__show');
-   });
 });
