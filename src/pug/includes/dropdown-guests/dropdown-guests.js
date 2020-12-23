2dbfec3ff64fdf48887fa26056e6a783d3f7be48
Sprashivaite <sanitar.kem@mail.ru>
2020-12-23 16:19:29 +0700

    переделан блок

--- src/pug/includes/dropdown-guests/dropdown-guests.js
+++ src/pug/includes/dropdown-guests/dropdown-guests.js
@@ -1,151 +1,58 @@
-/* global jQuery */
-
-// plugin styles
-
-
-/* eslint-disable func-names */
-(function ($) {
-  const defaults = {
-    maxItems: Infinity,
-    minItems: 0,
-    selectionText: 'item',
-    textPlural: 'items',
-    controls: {
-      position: 'right',
-      displayCls: 'iqdropdown-content',
-      controlsCls: 'iqdropdown-item-controls',
-      counterCls: 'counter',
-    },
-    items: {},
-    onChange: () => {},
-    beforeDecrement: () => true,
-    beforeIncrement: () => true,
-    setSelectionText (itemCount, totalItems) {
-      const usePlural = totalItems !== 0 && this.textPlural.length > 0;
-      const text = usePlural ? this.textPlural : this.selectionText;
-      if (totalItems == 0) {totalItems = ''; $('.iqdropdown__clear .button__text_empty').hide();}
-      else {$('.iqdropdown__clear .button__text_empty').show();}
-      return `${totalItems} ${text}`;
-    },
-  };
-
-  $.fn.iqDropdown = function (options) {
-    this.each(function () {
-      const $this = $(this);
-      const $selection = $this.find('p.iqdropdown-selection').last();
-      const $menu = $this.find('div.iqdropdown-menu');
-      const $items = $menu.find('div.iqdropdown-menu-option');
-      const dataAttrOptions = {
-        selectionText: $selection.data('selection-text'),
-        textPlural: $selection.data('text-plural'),
-      };
-      const settings = $.extend(true, {}, defaults, dataAttrOptions, options);
-      const itemCount = {};
-      let totalItems = 0;
-
-      function updateDisplay () {
-        $selection.html(settings.setSelectionText(itemCount, totalItems));
-      }
-
-      function setItemSettings (id, $item) {
-        const minCount = Number($item.data('mincount'));
-        const maxCount = Number($item.data('maxcount'));
-
-        settings.items[id] = {
-          minCount: Number.isNaN(Number(minCount)) ? 0 : minCount,
-          maxCount: Number.isNaN(Number(maxCount)) ? Infinity : maxCount,
-        };
-      }
-
-      function addControls (id, $item) {
-        const $controls = $('<div />').addClass(settings.controls.controlsCls);
-        const $decrementButton = $(`
-          <button class="button-decrement">
-            <i class="icon-decrement"></i>
-          </button>
-        `);
-        const $incrementButton = $(`
-          <button class="button-increment">
-            <i class="icon-decrement icon-increment"></i>
-          </button>
-        `);
-        const $counter = $(`<span>${itemCount[id]}</span>`).addClass(settings.controls.counterCls);
-
-        $item.children('div').addClass(settings.controls.displayCls);
-        $controls.append($decrementButton, $counter, $incrementButton);
-
-        if (settings.controls.position === 'right') {
-          $item.append($controls);
-        } else {
-          $item.prepend($controls);
-        }
-        $('.iqdropdown__clear').click(function(){
-            totalItems = 0;
-            itemCount[id] = 0;
-            $counter.html(itemCount[id]);
-            updateDisplay();
-          });
-        $decrementButton.click((event) => {
-          const { items, minItems, beforeDecrement, onChange } = settings;
-          const allowClick = beforeDecrement(id, itemCount);
-
-          if (allowClick && totalItems > minItems && itemCount[id] > items[id].minCount) {
-            itemCount[id] -= 1;
-            totalItems -= 1;
-            $counter.html(itemCount[id]);
-            updateDisplay();
-            onChange(id, itemCount[id], totalItems);
-          }
-
-          event.preventDefault();
-        });
-
-        $incrementButton.click((event) => {
-          const { items, maxItems, beforeIncrement, onChange } = settings;
-          const allowClick = beforeIncrement(id, itemCount);
-
-          if (allowClick && totalItems < maxItems && itemCount[id] < items[id].maxCount) {
-            itemCount[id] += 1;
-            totalItems += 1;
-            $counter.html(itemCount[id]);
-            updateDisplay();
-            onChange(id, itemCount[id], totalItems);
-          }
-
-          event.preventDefault();
-        });
-
-        $item.click(event => event.stopPropagation());
-
-        return $item;
-      }
-
-      $this.click(() => {
-        $this.toggleClass('menu-open');
-      });
-      $items.each(function () {
-        const $item = $(this);
-        const id = $item.data('id');
-        const defaultCount = Number($item.data('defaultcount') || '0');
-
-        itemCount[id] = defaultCount;
-        totalItems += defaultCount;
-        setItemSettings(id, $item);
-        addControls(id, $item);
-      });
-
-      updateDisplay();
-    });
-
-    return this;
-  };
-}(jQuery));
-
-
-$(function() {
-  $('.iqdropdown').iqDropdown({
-    selectionText: 'Сколько гостей',
-    textPlural: 'гостей',
+$(document).ready(function () {
+  $('.minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 0 ? 0 : count;
+      $input.val(count);
+      $input.change();
+      return false;
   });
-});
+  $('.plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+  });
 
+  function declOfNum(number, titles) {
+      cases = [2, 0, 1, 1, 1, 2];
+      return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }  
+      $('.dropdown-content__box-button').find('button').click(function () {
+          let target = $(this).closest('.dropdown')
+          let child = target.find('.child').val();
+          let adult = target.find('.adult').val();
+          let baby = target.find('.baby').val();
+          let summ = parseInt(child) + parseInt(adult) + parseInt(baby);
+
+          if(summ === 0){
+              target.find('.dropbtn__text').text('Сколько гостей');
+             
+          } else {target.find('.dropbtn__text').text(summ + ' ' + declOfNum(summ, ['гость', 'гостя', 'гостей']));target.find('.clear').css('opacity', '1');}
+            
+      });
+ 
+  function resizeInput() {
+      $(this).attr('size', $(this).val().length);
+  }
+  $('input[type="text"]') 
+      .keyup(resizeInput) 
+      .each(resizeInput); 
+  $('.dropdown__dropbtn').click(function () {
+      if($(this).next('.dropdown-content').hasClass('dropdown-content__show')){
+          $(this).next('.dropdown-content').removeClass('dropdown-content__show');
+      } else $(this).next('.dropdown-content').addClass('dropdown-content__show');
+
+  }); 
+  $('.dropdown__buttons-container .clear').click(function () {
+    $('.dropdown__buttons-container .clear').css('opacity', '0');
+      $('.dropbtn__text').text('Сколько гостей');
+      $('.child').val(0);
+      $('.adult').val(0);
+      $('.baby').val(0);
+  
+  });
+  $('.dropdown__buttons-container .submit').click(function () {
+      $('.dropdown-content').removeClass('dropdown-content__show');
+   });
+});
