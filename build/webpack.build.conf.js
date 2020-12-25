8785fbd831f6361cc9d4a5421d1c4bc170c492e2
Sprashivaite <sanitar.kem@mail.ru>
2020-12-25 23:48:45 +0700

    удалены комментарии

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -3,7 +3,6 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-  // BUILD config
   mode: 'production',
   plugins: []
 })
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,7 +3,6 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-  // BUILD config
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
