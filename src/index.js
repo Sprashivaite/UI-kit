1e83fefbda3ae5670af4fe90011c99578fd78be0
Sprashivaite <sanitar.kem@mail.ru>
2020-12-25 23:46:37 +0700

    добавлены rem

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import './styl/main.styl'
 
-
-import ionRangeSlider from 'ion-rangeslider'
-// import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
-// import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
 import './pug/includes/range-slider/range-slider.js'
 import "./pug/includes/rate-button/rate-button.js"
 import "./pug/includes/like-button/like-button.js"
 import "./pug/includes/pagination/pagination.js"
 import "./pug/includes/calendar/calendar.js"
+import "./pug/includes/dropdown-facilities/dropdown-facilities.js"
 import "./pug/includes/dropdown-guests/dropdown-guests.js"
 import "./pug/includes/data-dropdown/data-dropdown.js"
-import "./pug/includes/dropdown-facilities/dropdown-facilities.js"
+
 import "./pug/includes/room-card/room-card.js"
 
 
