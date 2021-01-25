ea5f63f2fac1b9eb32036aff7d17db0a85df553a
Sprashivaite <sanitar.kem@mail.ru>
2021-01-25 11:13:12 +0700

    Добавлены js- классы

--- src/components/dropdown-guests/dropdown-guests.js
+++ src/components/dropdown-guests/dropdown-guests.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
-  $('.dropdown .minus').click(function () {
+  $('.js-dropdown .js-minus').click(function () {
     const $input = $(this).parent().find('input');
-    const count = parseInt($input.val()) - 1;
+    let count = parseInt($input.val()) - 1;
     count = count < 0 ? 0 : count;
     $input.val(count);
     $input.change();
     return false;
   });
-  $('.dropdown .plus').click(function () {
+  $('.js-dropdown .js-plus').click(function () {
     const $input = $(this).parent().find('input');
     $input.val(parseInt($input.val()) + 1);
     $input.change();
@@ -22,42 +22,42 @@ $(document).ready(() => {
         : cases[number % 10 < 5 ? number % 10 : 5]
     ];
   }
-  $('.dropdown .dropdown-content__box-button')
+  $('.js-dropdown .js-dropdown-content__box-button')
     .find('button')
     .click(function () {
-      const target = $(this).closest('.dropdown');
+      const target = $(this).closest('.js-dropdown');
       const child = target.find('.child').val();
       const adult = target.find('.adult').val();
       const baby = target.find('.baby').val();
-      const summ = parseInt(child) + parseInt(adult) + parseInt(baby);
+      const sum = parseInt(child) + parseInt(adult) + parseInt(baby);
 
-      if (summ === 0) {
-        target.find('.dropbtn__text').text('Сколько гостей');
+      if (sum === 0) {
+        target.find('.js-dropbtn__text').text('Сколько гостей');
       } else {
         target
-          .find('.dropbtn__text')
-          .text(`${summ} ${declOfNum(summ, ['гость', 'гостя', 'гостей'])}`);
+          .find('.js-dropbtn__text')
+          .text(`${sum} ${declOfNum(sum, ['гость', 'гостя', 'гостей'])}`);
         target.find('.clear').css('opacity', '1');
       }
 
-      target.find('.clear').click(() => {
+      target.find('.js-clear').click(() => {
         target.find('.clear').css('opacity', '0');
-        target.find('.dropbtn__text').text('Сколько гостей');
+        target.find('.js-dropbtn__text').text('Сколько гостей');
         target.find('.child').val(0);
         target.find('.adult').val(0);
         target.find('.baby').val(0);
       });
-      target.find('.submit').click(() => {
-        target.find('.dropdown-content').removeClass('dropdown-content__show');
+      target.find('.js-submit').click(() => {
+        target.find('.js-dropdown-content').removeClass('dropdown-content__show');
       });
     });
 
-  $('.dropdown .dropdown__dropbtn').click(function () {
-    if ($(this).next('.dropdown-content').hasClass('dropdown-content__show')) {
-      $(this).next('.dropdown-content').removeClass('dropdown-content__show');
+  $('.js-dropdown .js-dropdown__dropbtn').click(function () {
+    if ($(this).next('.js-dropdown-content').hasClass('dropdown-content__show')) {
+      $(this).next('.js-dropdown-content').removeClass('dropdown-content__show');
       $(this).find('i').css('transform', 'rotate(0deg)');
     } else {
-      $(this).next('.dropdown-content').addClass('dropdown-content__show');
+      $(this).next('.js-dropdown-content').addClass('dropdown-content__show');
       $(this).find('i').css('transform', 'rotate(180deg)');
     }
   });
