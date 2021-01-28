c53df214c2087835ae1a285a9e125315d772db97
Sprashivaite <sanitar.kem@mail.ru>
2021-01-28 16:10:52 +0700

    Добавлено выпадающее меню второму полю

--- src/components/data-dropdown/data-dropdown.js
+++ src/components/data-dropdown/data-dropdown.js
@@ -4,3 +4,9 @@ $('#data-dropdown-1').datepicker({
     $('#data-dropdown-2').val(fd.split('-')[1]);
   },
 });
+$('#data-dropdown-2').datepicker({
+  onSelect(fd) {
+    $('#data-dropdown-1').val(fd.split('-')[0]);
+    $('#data-dropdown-2').val(fd.split('-')[1]);
+  },
+});
