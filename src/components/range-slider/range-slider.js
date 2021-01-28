aef6432593a3a18dabc59e3e0e6e61071ef616c1
Sprashivaite <sanitar.kem@mail.ru>
2021-01-28 19:19:59 +0700

    Исправлено eslint

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,24 +1,24 @@
-$(function() {
-    let $range = $(".js-range-slider-input");
-    let $value = $(".js-range-slider__values");
-    $range.ionRangeSlider({
-        type: "double",
-        skin: "big",
-        min: 0,
-        max: 15000,
-        from: 5000,
-        to: 10000,
-        postfix: '₽',
-        prettify_enabled: true, 
-        hide_min_max: true,
-        force_edges: true,
-        onStart: function() { 
-            $value.text($(".irs-from").text() + " - " + $(".irs-to").text());
-        },
-        onChange: function() {
-            $value.text($(".irs-from").text() + " - " + $(".irs-to").text());
-        }
-    }); 
-    $value.text($(".irs-from").text() + " - " + $(".irs-to").text());
-    $value.text($(".irs-from").text() + " - " + $(".irs-to").text());
+$(function () {
+  const $range = $('.js-range-slider-input');
+  const $value = $('.js-range-slider__values');
+  $range.ionRangeSlider({
+    type: 'double',
+    skin: 'big',
+    min: 0,
+    max: 15000,
+    from: 5000,
+    to: 10000,
+    postfix: '₽',
+    prettify_enabled: true,
+    hide_min_max: true,
+    force_edges: true,
+    onStart() {
+      $value.text(`${$('.irs-from').text()} - ${$('.irs-to').text()}`);
+    },
+    onChange() {
+      $value.text(`${$('.irs-from').text()} - ${$('.irs-to').text()}`);
+    },
+  });
+  $value.text(`${$('.irs-from').text()} - ${$('.irs-to').text()}`);
+  $value.text(`${$('.irs-from').text()} - ${$('.irs-to').text()}`);
 });
