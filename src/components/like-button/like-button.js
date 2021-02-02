32d96ab94974b106492d15cf0aff033495110f16
Sprashivaite <sanitar.kem@mail.ru>
2021-02-02 21:44:26 +0700

    Добавлен visually-hidden.  Исправлены стили. #9

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,22 +1,21 @@
 const changeValueLike = function changeValueLike(buttons) {
   buttons.forEach((button) => {
     button.onmousedown = () => false;
-    const input = button.querySelector('.js-like-button');
+    const input = button.querySelector('.js-like-button__input');
     const value = button.querySelector('.js-like-button__value');
     const add = function add() {
       if (input.checked) {
-        value.innerHTML = +value.innerHTML + 1;
+        value.innerHTML = Number(value.innerHTML) + 1;
       }
     };
     const deleteLike = function deleteLike() {
       if (!input.checked) {
-        value.innerHTML = +value.innerHTML - 1;
+        value.innerHTML = Number(value.innerHTML) - 1;
       }
     };
     input.addEventListener('click', add);
     input.addEventListener('click', deleteLike);
   });
 };
-
-const likeButtons = document.querySelectorAll('.like-button-container');
+const likeButtons = document.querySelectorAll('.js-like-button');
 changeValueLike(likeButtons);
