862a6d3dc06d3e1a4e15d93011c6c29545c77f7b
Sprashivaite <sanitar.kem@mail.ru>
2021-03-22 07:09:03 +0700

    added favicon for gh-pages

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -128,6 +128,8 @@ module.exports = {
         new FaviconsWebpackPlugin({
             logo: './src/favicon.svg',
             outputPath: '/assets/favicon',
+            publicPath: '',
+            prefix: 'assets/favicon',
         }),
         ...PAGES.map(page => new HtmlWebpackPlugin({
             template: `${PAGES_DIR}/${page}/${page}.pug`,
