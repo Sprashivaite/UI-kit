4dfa0fc9dc0cf3a99beefa28ec6b50a50a95b515
Sprashivaite <sanitar.kem@mail.ru>
2021-03-29 12:51:56 +0700

    fixed postCss warning #31

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,33 +2,33 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const webpack = require('webpack')
-const fs = require('fs')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const webpack = require('webpack');
+const fs = require('fs');
 const ghpages = require('gh-pages');
 
 const PATHS = {
-    src: path.join(__dirname, '../src'),
-    dist: path.join(__dirname, '../dist'),
-    assets: 'assets/'
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/',
 };
 
-const PAGES_DIR = `${PATHS.src}/pages`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName)
+const PAGES_DIR = `${PATHS.src}/pages`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter((fileName) => fileName);
 
 module.exports = {
-    externals: {
-        paths: PATHS
-    },
-    entry: {
-        app: PATHS.src
-    },
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: PATHS.src,
+  },
 
-    output: {
-        filename: `${PATHS.assets}js/[name].[hash].js`,
-        path: PATHS.dist,
-        // publicPath: '/'
-    },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    // publicPath: '/'
+  },
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -36,105 +36,107 @@ module.exports = {
           name: 'vendors',
           test: /node_modules/,
           chunks: 'all',
-          enforce: true
-        }
-      }
-    }
-},
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: '/node_modules/',
-                query: {
-                    cacheDirectory: true
-                }
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader'
-              },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]'
-                }
-            }, 
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]'
-                }
-              },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    {loader: MiniCssExtractPlugin.loader,
-                        options: {publicPath: '../'}
-                        },
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true, url: false }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: { sourceMap: true, config: { path: `./postcss.config.js` }, url: false }
-                      },
-                    {
-                        loader: 'stylus-loader',
-                        options: { sourceMap: true, url: false }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {loader: MiniCssExtractPlugin.loader,
-                    options: {publicPath: '../'}
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true, url: false}
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: { sourceMap: true,url: false, config: { path: `./postcss.config.js` } }
-                      },
-                ]
-            }
-        ]
+          enforce: true,
+        },
+      },
     },
-    devServer: {
-        overlay: true
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: `${PATHS.assets}css/[name].[contenthash].css`
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-        }),
-        new CopyWebpackPlugin([
-            { from: `${PATHS.src}/**/*.{png,jpg,gif,svg}`, to: `${PATHS.assets}img/[name].[ext]`},
-            { from: `${PATHS.src}/fonts`, to: `${PATHS.assets}fonts` },
-        ]),
-        new FaviconsWebpackPlugin({
-            logo: './src/favicon.svg',
-            outputPath: '/assets/favicon',
-            publicPath: '',
-            prefix: 'assets/favicon',
-        }),
-        ...PAGES.map(page => new HtmlWebpackPlugin({
-            template: `${PAGES_DIR}/${page}/${page}.pug`,
-            filename: `${page}.html`,
-          })),  
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        query: {
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '../' },
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, url: false },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: './postcss.config.js' } },
+          },
+          {
+            loader: 'stylus-loader',
+            options: { sourceMap: true, url: false },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '../' },
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, url: false },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: './postcss.config.js' } },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    overlay: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[contenthash].css`,
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/**/*.{png,jpg,gif,svg}`, to: `${PATHS.assets}img/[name].[ext]` },
+      { from: `${PATHS.src}/fonts`, to: `${PATHS.assets}fonts` },
+    ]),
+    new FaviconsWebpackPlugin({
+      logo: './src/favicon.svg',
+      outputPath: '/assets/favicon',
+      publicPath: '',
+      prefix: 'assets/favicon',
+    }),
+    ...PAGES.map((page) => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}/${page}.pug`,
+      filename: `${page}.html`,
+    })),
+  ],
 };
-ghpages.publish('dist', function(err) {});
+ghpages.publish('dist', (err) => {});
