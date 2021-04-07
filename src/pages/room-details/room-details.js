e60c31a2ce563ee878c5d5233fec2fec68c4cf4b
Sprashivaite <sanitar.kem@mail.ru>
2021-04-07 23:28:08 +0700

    renamed folder

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -2,12 +2,12 @@ import LikeButton from '../../components/like-button/like-button';
 import Dropdown from '../../components/dropdown/dropdown';
 import initDataDouble from '../../components/date-range/date-range';
 
-if (document.querySelector('.js-room-details-page')) {
+if (document.querySelector('.js-room-details')) {
   const likeButtons = document.querySelectorAll('.js-like-button');
   likeButtons.forEach((button) => {
     new LikeButton().findElements(button).addHandler();
   });
   const guestsDropdown = document.querySelector('.js-dropdown');
   new Dropdown(guestsDropdown, [['гостей', 'гость', 'гостя']]).findElements().addHandler();
-  initDataDouble($('.js-room-details-page__reservation')); 
+  initDataDouble($('.js-room-details__reservation')); 
 }
