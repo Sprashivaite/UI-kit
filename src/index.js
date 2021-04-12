e5576f583d890a5fdaccfcb9f3f0f4e9e0df581e
Sprashivaite <sanitar.kem@mail.ru>
2021-04-12 16:44:58 +0700

    deleted favicon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styl/main.styl';
+
 import './pages/form-elements/form-elements';
 import './pages/cards/cards';
 import './pages/registration-page/registration-page';
 import './pages/landing-page/landing-page';
 import './pages/search-room/search-room';
 import './pages/room-details/room-details';
+// import './favicon/favicon';
