16721403b95ea088a81d201ed0f9276980ef3883
Sprashivaite <sanitar.kem@mail.ru>
2021-04-28 23:03:48 +0700

    added stylelintrc and fixed style problems

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    'stylelint-plugin-stylus/standard',
+  ],
+  rules: {
+    "rule-empty-line-before": "never",
+    "stylus/at-rule-empty-line-before": "never"
+  },
+};
