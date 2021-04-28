c8fcb0f828fb45485a2b65722ca5f8380e312749
Sprashivaite <sanitar.kem@mail.ru>
2021-04-28 23:44:26 +0700

    added rules and fixed problems

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   rules: {
     "rule-empty-line-before": "never",
-    "stylus/at-rule-empty-line-before": "never"
+    "stylus/at-rule-empty-line-before": "never",
+    "no-descending-specificity": null,
+    "font-family-no-missing-generic-family-keyword": [
+      true,
+      {
+        ignoreFontFamilies: ["Material Icons"]
+      }
+    ]
   },
 };
